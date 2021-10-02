24aab36f5ffb22e8df99ee5805a4764ff44f8f65
Adrian Halaburda <adh761@gmail.com>
2021-10-02 00:20:20 -0400

    Chapter 1 intros added

---
+++ frontend/src/intros/en/Chapter1B.js
@@ -0,0 +1,123 @@
+import React from 'react';
+
+
+function Chapter1BEN() {
+	return (
+        <div classname="container">
+        <h3>
+          <strong>
+            <em>Numbers</em>
+          </strong>
+        </h3>
+        <br />
+        <p>We are going to learn numbers in Guarani del 1 al 10</p>
+        <table classname="table table-responsive table-striped table-bordered">
+          <thead classname="thead-dark">
+            <tr>
+              <th>
+                <p>#</p>
+              </th>
+              <th classname="text-center">
+                <p>Number</p>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td classname="text-center">
+                <p>1</p>
+              </td>
+              <td classname="text-center">
+                <p>Peteĩ</p>
+              </td>
+            </tr>
+            <tr></tr>
+            <tr>
+              <td classname="text-center">
+                <p>2</p>
+              </td>
+              <td classname="text-center">
+                <p>Mokõi</p>
+              </td>
+            </tr>
+            <tr></tr>
+            <tr>
+              <td classname="text-center">
+                <p>3</p>
+              </td>
+              <td classname="text-center">
+                <p>Mbohapy</p>
+              </td>
+            </tr>
+            <tr></tr>
+            <tr>
+              <td classname="text-center">
+                <p>4</p>
+              </td>
+              <td classname="text-center">
+                <p>Irundy</p>
+              </td>
+            </tr>
+            <tr></tr>
+            <tr>
+              <td classname="text-center">
+                <p>5</p>
+              </td>
+              <td classname="text-center">
+                <p>Po</p>
+              </td>
+            </tr>
+            <tr></tr>
+            <tr>
+              <td classname="text-center">
+                <p>6</p>
+              </td>
+              <td classname="text-center">
+                <p>Poteĩ</p>
+              </td>
+            </tr>
+            <tr></tr>
+            <tr>
+              <td classname="text-center">
+                <p>7</p>
+              </td>
+              <td classname="text-center">
+                <p>Pokõi</p>
+              </td>
+            </tr>
+            <tr></tr>
+            <tr>
+              <td classname="text-center">
+                <p>8</p>
+              </td>
+              <td classname="text-center">
+                <p>Poapy</p>
+              </td>
+            </tr>
+            <tr></tr>
+            <tr>
+              <td classname="text-center">
+                <p>9</p>
+              </td>
+              <td classname="text-center">
+                <p>Porundy</p>
+              </td>
+            </tr>
+            <tr></tr>
+            <tr>
+              <td classname="text-center">
+                <p>10</p>
+              </td>
+              <td classname="text-center">
+                <p>Pa</p>
+              </td>
+            </tr>
+            <tr></tr>
+          </tbody>
+        </table>
+      </div>
+      
+	);
+}
+
+export default Chapter1BEN;
---
+++ frontend/src/intros/es/Chapter1C.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function Chapter1C() {
+	return (
+        <div classname="container">
+          <h3>
+            <strong>
+              <em> Pronombres personales – Países </em>
+            </strong>
+          </h3>
+          <br />
+          <p><strong>Terarăngue – Pronombres:</strong></p>
+          <p>El pronombre es la palabra que sustituye al nombre de la persona, el animal o la cosa, dentro de un hecho conocido por el cual habla y el oye. Se lo denomina “marca” o “sustituto.</p>
+          <p><strong>Terarãnguete teroñe’ẽramo – Pronombre personal como sujeto: </strong></p>
+          <p>Sustituye a las tres personas gramaticales del singular y del plural.</p>
+          <br />
+          <br />
+          <hr />
+          <p><strong>Singular:</strong> Che – Nde - Ha´e.</p>
+          <p><strong>Plural:</strong> Ñande – Ore – Pe´ĕ -Ha´ekuéra.</p>
+        </div>
+	);
+}
+
+export default Chapter1C;
