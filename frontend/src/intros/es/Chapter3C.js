cfb0681fe7104292dc9a3c4cfd1dd9fa772d36a3
Adrian H <adh761@gmail.com>
2021-10-11 17:15:28 -0300

    Some intros fix

--- frontend/src/intros/es/Chapter3C.js
+++ frontend/src/intros/es/Chapter3C.js
@@ -16,3 +16,20 @@ function Chapter3C() {
 }
 
 export default Chapter3C;
+
+// 1A. Mba’éichapa
+// 1B. Peteĩ, mokõi, mbohapy...
+// 1C. Che ha nde.
+// 1D. Peẽ ha ha’ekuéra.
+// 2A. Ryguasukuéra ha jaguaita, umíva.
+// 2B. Che sy.
+// 2C. Óga porã, morotĩ.
+// 3A. Aguata ha jaguata.
+// 3B. Reguata ha peguata.
+// 3C. Oguata.
+// 4A. Mboyve ha rire.
+// 4B. Pomberto ha Luisón. Máva.
+// 4C. Kamisa ha kasõ.
+// 5A. Apuraheiva’ekue ha apukákuri.
+// 5B. Ahákuri.
+// 5C. Apu’ã ha aguapy.
